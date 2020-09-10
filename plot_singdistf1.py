a7881896fefadd3a5f9a9e95117d03c6bace8996
Subramanian Lakshminarasimhan <38849557+lnannu96@users.noreply.github.com>
2020-09-10 13:14:04 -0400

    Altered F1 DistSing Plot

--- plot_singdistf1.py
+++ plot_singdistf1.py
@@ -8,8 +8,8 @@ singdist_patch1 = data[:,2]
 singdist_patch3 = data[:,3]
 
 
-plt.plot(phi,singdist_patch1,'b-',label='Patch $z_1$')
-plt.plot(phi,singdist_patch3,'r-',label='Patch $z_3$')
+#plt.plot(phi,singdist_patch1,'b-',label='Patch $z_1$')
+plt.plot(phi,singdist_patch3,'b-',label='Patch $z_3$')
 plt.xlabel('$\phi$')
 plt.ylabel('Distance in CPN')
 plt.title('Distance from Singularity $f_1$ $\psi=-0.5$')
